bf641f83bed1ca8f5b6731624a99bd46e8568e34
Rainulf Pineda <rainulf@rainulf.ca>
2012-10-30 18:46:28 -0400

    index.php for the anime club.

--- index.php
+++ index.php
@@ -1,12 +1,56 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <script src="processing.js"></script>
-</head>	
-<body>
-	<h1>Japanese drilling v1.5</h1>
-    <p> still improving</p>
-    <canvas data-processing-sources="canvas.pde"></canvas>
-	
-</body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+
+	<head>
+		<title>Seneca@York Anime Club - Japanese drilling</title>
+		<meta name="description" content="Seneca@York Anime Club's Japanese Syllabary Translator." />
+		<meta name="keywords" content="seneca college anime club, Japanese Syllabary Translator, translator, hiragana, katakana" />
+		<link rel="stylesheet" href="/style.css" type="text/css" />
+      <script src="processing.js"></script>
+
+	</head>
+
+	<body>
+
+		<table width="629" border="0" cellpadding="0" cellspacing="0" align="center">
+			<tr>
+				<td colspan="1"><img alt="image1" src="/image_001.jpg" width="629" height="213" /></td>
+			</tr>
+			<tr>
+				<td colspan="1"><img alt="image2" src="/image_002.jpg" width="629" height="215" /></td>
+			</tr>
+
+			<tr>
+				<td id="content">
+					<!-- Navigation -->
+					<center>
+						<a href="http://syanimeclub.ca" class="nav">Home</a> | 
+						<a href="https://www.google.com/calendar/b/0/embed?src=seneca.york.anime@gmail.com&ctz=America/Toronto" class="nav">Calendar</a>
+						<a href="http://twitter.com/syanimeclub" class="nav">Twitter</a> | 
+						<a href="https://www.facebook.com/pages/SenecaYork-Anime-Club/205557119513656" class="nav">Facebook</a> | 
+						<a href="mailto:seneca.york.anime@gmail.com" class="nav">Email</a> |
+						<a href="http://syanimeclub.ca/onsetsu.php" class="nav">Onsetsu</a> | 
+                  <a href="http://syanimeclub.ca/japanese_drilling" class="nav">Japanese drilling</a>
+					</center>
+
+					<!--Start content-->
+					<h1>Japanese drilling</h1>
+					<div class="display" style="text-align:center;">
+						<canvas data-processing-sources="canvas.pde"></canvas>
+
+					</div>
+
+
+					<h1>Credits</h1>
+					<div class="display" style="text-align: center;">
+						v1.5. <b>By:</b> <a href="https://github.com/wwang109/japanese_drilling"> wwang109</a><br />
+					</div>
+				</td>
+			</tr>
+
+
+		</table>
+	</body>
 </html>
