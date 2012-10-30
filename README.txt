a718de4b9c5859279983bf5cbc110969b8c70af7
Wen <wen.wang2@gmail.com>
2012-10-30 15:01:49 -0400

    readme

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
-version 1.0 released - 10/26/2012
+version 1.5 released - 10/30/2012
 
+version 1.0 released - 10/26/2012
 -alot of bug
--- index.php
+++ index.php
@@ -4,8 +4,8 @@
   <script src="processing.js"></script>
 </head>	
 <body>
-	<h1>Japanese drilling v1.0</h1>
-    <p>Need improvements</p>
+	<h1>Japanese drilling v1.5</h1>
+    <p> still improving</p>
     <canvas data-processing-sources="canvas.pde"></canvas>
 	
 </body>
